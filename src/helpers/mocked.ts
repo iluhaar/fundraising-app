40b9b4b63d961c2e10761ec8d726476b4085c3b0
iluhaar <iluhaar@gmail.com>
2024-03-24 21:54:57 +0200

    fixes adjust styles add mocked data

---
+++ src/helpers/mocked.ts
@@ -0,0 +1,66 @@
+export const fetchMocked = [
+  {
+    bank: "Приват Конверт",
+    title: "На добру справу",
+    description:
+      "Хлопці на кремінському напрямку потребують DJI Mavic 3T для ефективного виконання завдань в ночі.",
+    amount: "65 568,14",
+    goal: "205 000,00",
+    closed: false,
+    ownerName: "Ткаченко Є.",
+    ownerIcon: "donat3.jpg",
+    jarId: "",
+  },
+  {
+    bank: "Монобанка",
+    title: "На DJI Mavic 3T",
+    description:
+      "Хлопці на кремінському напрямку потребують DJI Mavic 3T для ефективного виконання завдань в ночі. ",
+    amount: "17 296,00",
+    closed: false,
+    ownerName: "Єлисей Т.",
+    goal: "205 000,00",
+    ownerIcon:
+      "https://jar-img.monobank.com.ua/N92.30.0025660119.jpg?ts=2024-03-14T23:09:05.031820844",
+    jarId: "6A83s4Ugks",
+  },
+];
+
+export const postMocked = [
+  {
+    bank: "Монобанка",
+    title: "Авто для 453 ОПБ",
+    description: "",
+    amount: "52 451,10",
+    closed: false,
+    ownerName: "Данило Б.",
+    goal: "200 000,00",
+    ownerIcon:
+      "https://jar-img.monobank.com.ua/N92.30.0025689351.jpg?ts=2024-03-18T19:01:03.650437745",
+    jarId: "2hzMpRMGSC",
+  },
+  {
+    bank: "Монобанка",
+    title: "🚘 для БпАК «Довбуш»",
+    description: "",
+    amount: "2 400,00",
+    closed: false,
+    ownerName: "Ілля Р.",
+    goal: "5 000,00",
+    ownerIcon:
+      "https://ava-img.monobank.com.ua/NqJAEHONJZ4lEaerJztWu32dI-B1dnxzmKNOVywcKFY=.jpg",
+    jarId: "51TGTkuK3k",
+  },
+  {
+    bank: "Монобанка",
+    title: "🚘 для БпАК «Довбуш»",
+    description: "",
+    amount: "1 645,00",
+    closed: false,
+    ownerName: "Віолета Ф.",
+    goal: "5 000,00",
+    ownerIcon:
+      "https://ava-img.monobank.com.ua/NqJAEHONJZ4lEaerJztWu_HlK3sLDATNoPioH8C0cXQ=.jpg",
+    jarId: "7sKE5pNsUb",
+  },
+];
